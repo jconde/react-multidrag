328e88b1ea6ad0c60f3d48ae4f74c861d5cc9bc3
Javier Conde <jcondem@gmail.com>
2016-10-01 18:27:38 -0500

    Added travis and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+  output: {
+    library: 'react-multidrag',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom"
+      }
+    }
+  ]
+};
